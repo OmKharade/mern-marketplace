b6d50bc78d52cd312d0356a0b61fda61620f6fed
OmKharade <133861833+OmKharade@users.noreply.github.com>
2023-11-10 02:26:56 -0500

    complete header search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,27 @@
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
   const {currentUser} = useSelector(state=>state.user)
+  const [searchTerm, setSearchTerm] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  }
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(window.location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if (searchTermFromUrl)  setSearchTerm(searchTermFromUrl)
+  },[])
+
   return (
     <header className='border'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -13,9 +31,11 @@ export default function Header() {
             <span>Place</span>
         </h1>
         </Link>
-        <form className='bg-gray-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-gray-500'/>
+        <form onSubmit={handleSubmit} className='bg-gray-100 p-3 rounded-lg flex items-center'>
+            <input onChange={(e) => setSearchTerm(e.target.value)} type="text" placeholder='Search' value={searchTerm} className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+            <button>
+              <FaSearch className='text-gray-500'/>
+            </button>
         </form>
         <ul className='flex gap-4'>
             <Link to='/'><li className='hidden sm:inline hover:underline underline-offset-8'>Home</li></Link>
